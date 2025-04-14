7b613df00fb73a5d49cff8fd3df44898872e3ba3
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-14 00:13:38 -0300

    feat: ✨ add register controller

---
+++ src/http/controllers/users/register.controller.ts
@@ -0,0 +1,48 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+
+import { z } from 'zod'
+
+import { makeRegisterUseCase } from '../../../use-cases/users/factories/make-register'
+
+import { UsernameAlreadyExistsError } from '../../../use-cases/errors/username-already-exists'
+
+import { EmailAlreadyExistsError } from '../../../use-cases/errors/email-already-exists'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    firstName: z.string(),
+    lastName: z.string().optional(),
+    bio: z.string().optional(),
+    username: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { firstName, lastName, bio, username, email, password } =
+    registerBodySchema.parse(request.body)
+
+  try {
+    const registerUseCase = makeRegisterUseCase()
+
+    await registerUseCase.execute({
+      firstName,
+      lastName,
+      bio,
+      username,
+      email,
+      password,
+    })
+  } catch (error) {
+    if (error instanceof UsernameAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    if (error instanceof EmailAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  return reply.status(201).send()
+}
