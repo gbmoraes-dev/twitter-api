0c4359c72fb469b3a9c3674fa222ae8bce80e2dd
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-13 21:39:51 -0300

    fix: 🛠️ update production logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,16 @@
 import { pino } from 'pino'
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'yyyy-mm-dd HH:MM:ss Z',
-      ignore: 'pid,hostname',
-    },
-  },
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-})
+export const logger = pino(
+  process.env.NODE_ENV === 'development'
+    ? {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'yyyy-mm-dd HH:MM:ss Z',
+            ignore: 'pid,hostname',
+          },
+        },
+      }
+    : {},
+)
