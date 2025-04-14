7f706f05bfdf621643b003eb658e6a2372f9f2f5
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-13 23:05:57 -0300

    feat: ✨ add users repository interface

---
+++ src/repositories/users.repository.ts
@@ -0,0 +1,11 @@
+import type { PublicUser, User, UserInsert } from '@/db/schemas'
+
+export interface UsersRepository {
+  create(data: UserInsert): Promise<User>
+  findById(id: string): Promise<User | null>
+  findByEmail(email: string): Promise<User | null>
+  findByUsername(username: string): Promise<User | null>
+  search(query: string, page: number): Promise<PublicUser[]>
+  update(id: string, data: Partial<UserInsert>): Promise<User | null>
+  delete(id: string): Promise<boolean>
+}
