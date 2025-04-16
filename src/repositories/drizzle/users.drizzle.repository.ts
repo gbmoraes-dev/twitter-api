b71e8cb772ff2b3d605e7b8dcb81b284151b6ef8
gbmoraes-dev <gbmoraes.dev@gmail.com>
2025-04-16 11:41:19 -0300

    feat: ✨ add drizzle find by token implementation

--- src/repositories/drizzle/users.drizzle.repository.ts
+++ src/repositories/drizzle/users.drizzle.repository.ts
@@ -39,6 +39,15 @@ export class UsersDrizzleRepository implements UsersRepository {
     return user
   }
 
+  async findByVerificationToken(token: string): Promise<User | null> {
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.emailVerificationToken, token))
+
+    return user
+  }
+
   async search(query: string, page: number): Promise<PublicUser[]> {
     const result = await db
       .select({
@@ -63,7 +72,7 @@ export class UsersDrizzleRepository implements UsersRepository {
     return result
   }
 
-  async update(id: string, data: Partial<UserInsert>): Promise<User | null> {
+  async update(id: string, data: Partial<UserInsert>): Promise<User> {
     const [user] = await db
       .update(users)
       .set({
